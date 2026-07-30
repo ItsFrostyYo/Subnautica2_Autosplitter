37bef1116bdb4f784d89d7133f435bc7ccf26539
ItsFrostyYo <Kaleb.Austin0009@gmail.com>
2026-07-30 07:48:12 -0400

    AHHH

--- LiveSplit.Subnautica2/Properties/AssemblyInfo.cs
+++ LiveSplit.Subnautica2/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.3.3")]
-[assembly: AssemblyFileVersion("1.3.3.3")]
+[assembly: AssemblyVersion("1.3.4.1")]
+[assembly: AssemblyFileVersion("1.3.4.1")]
 
 [assembly: ComponentFactory(typeof(Factory))]
